e61b7d38ccf6629ef012fac0bd53106934c927e6
Nikolay <nchernov@algotradinghub.com>
2019-03-24 23:32:57 +0200

    fix init sctipt

--- init.bat
+++ init.bat
@@ -1,9 +1,13 @@
 @echo off
 if not exist "%~dp0.venv" (
-  echo init virtual env
+  echo switch to develop branch...
+  git checkout develop
+  echo init virtual env...
   python -m venv .venv
   call %~dp0.venv\Scripts\activate.bat
+  echo upgrade pip...
   python -m pip install --upgrade pip
+  echo install requirements
   pip install -r requirements.txt
-  echo please manualy add env vars into %~dp0.venv\Scripts\activate.bat 
+  echo !!! please manualy add env vars into %~dp0.venv\Scripts\activate.bat !!! 
 )
